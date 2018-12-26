684c307121dae4c628dcc06617e9ce8269e653ab
iitmcvgmsi <Varun>
2018-12-26 21:30:06 +0530

    updated violence

---
+++ violence-detection-1/README.md
@@ -0,0 +1,53 @@
+![refer for more details here]()
+# Abnormal Crowd Detection
+Abnormal Crowd Detection Implementation with Python
+
+![Alt text](/picture1.png?raw=true "Function Pick")
+
+
+# Crowd Flow With Abnormal Simulation Video
+
+Please click on the image to navigate to the video.
+
+[![Watch the video](http://img.youtube.com/vi/6N8IgeqhNNI/0.jpg)](https://youtu.be/6N8IgeqhNNI)
+
+
+Please click on the image to navigate to the video.
+
+[![Watch the video](http://img.youtube.com/vi/rEJpO0L2Psg/0.jpg)](https://youtu.be/rEJpO0L2Psg)
+
+# Normal Crowd Flow
+
+Normal Crowd Flow Video 1
+
+[![Watch the video](http://img.youtube.com/vi/Bo0m3trfnj0/0.jpg)](https://youtu.be/Bo0m3trfnj0)
+
+Normal Crowd Flow Video 2
+
+[![Watch the video](http://img.youtube.com/vi/hGkHmI9sZx8/0.jpg)](https://youtu.be/hGkHmI9sZx8)
+
+Normal Crowd Flow Video 3
+
+[![Watch the video](http://img.youtube.com/vi/EyiaaLWZZM4/0.jpg)](https://youtu.be/EyiaaLWZZM4)
+
+Normal Crowd Flow Video 4
+
+[![Watch the video](http://img.youtube.com/vi/MN1MuQb9L6Y/0.jpg)](https://youtu.be/MN1MuQb9L6Y)
+
+Normal Crowd Flow Overflow
+
+Normal Crowd Flow Overflow Video 1
+
+[![Watch the video](http://img.youtube.com/vi/KYTwRdDtEJ8/0.jpg)](https://youtu.be/KYTwRdDtEJ8)
+
+Normal Crowd Flow Overflow Video 2
+
+[![Watch the video](http://img.youtube.com/vi/XXTl5A4ZDSg/0.jpg)](https://youtu.be/XXTl5A4ZDSg)
+
+Normal Crowd Flow Overflow Video 3
+
+[![Watch the video](http://img.youtube.com/vi/eW6_5UMhkB0/0.jpg)](https://youtu.be/eW6_5UMhkB0)
+
+Normal Crowd Flow Overflow Video 4
+
+[![Watch the video](http://img.youtube.com/vi/JrZ4JreQpoU/0.jpg)](https://youtu.be/JrZ4JreQpoU)
---
+++ violence-detection-1/validation_analysis.py
@@ -0,0 +1,76 @@
+import numpy as np
+import cv2
+import social_force_calculation
+import bag_of_words_analysis
+import lda
+import matplotlib.pyplot as plt
+import time
+
+
+'''
+This is the reproduction of "Abnormal Crowd Behavior Detection using Social Force Model" by Ramin Mehran, Alexis Oyama, Mubarak Shah
+The document is attached to project
+
+validation_analysis.py includes functionality testing approach
+
+References
+
+1. "Abnormal Crowd Behavior Detection using Social Force Model" by Ramin Mehran, Alexis Oyama, Mubarak Shah
+
+'''
+
+def show_data(videoUrl,resize):
+    xdata = []
+    ydata = []
+
+    plt.show()
+
+    axes = plt.gca()
+    axes.set_xlim(0, 6500)
+    axes.set_ylim(-4000, +0)
+    axes.set_xticks([1220,4650,6445], minor=False)
+    axes.set_xticks([420,520,1097,1495,1670,2170,2245,2670,2795,3270,3345,3995,4095,4520,4620,5145,5195,5695,5770,6395], minor=True)
+    axes.xaxis.grid(True, which='major',color='b', linestyle='-', alpha=0.8)
+    axes.xaxis.grid(True, which='minor',color='r', linestyle='-', alpha=0.2)
+    line, = axes.plot(xdata, ydata, 'r-')
+    cam = cv2.VideoCapture(videoUrl)
+    liks=np.load('LDAresult/likelihood.npy')
+    print (np.amax(liks), np.amin(liks))
+    j=0
+    i=0
+    count=0
+    while (cam.isOpened()):
+        ret, img = cam.read()
+        if not ret:
+            break
+        if(i>=liks.shape[0]):
+            break
+        print (liks[i])
+
+        xdata.append(count)
+        ydata.append(liks[i])
+        line.set_xdata(xdata)
+        line.set_ydata(ydata)
+        plt.draw()
+        plt.pause(1e-17)
+        time.sleep(0)
+        count+=1
+
+        if j==10:
+            i+=1
+            j=0
+        img = cv2.resize(img, (0, 0), fx=resize, fy=resize)
+
+        cv2.imshow('video', img)
+        ch = 0xFF & cv2.waitKey(5)
+        if ch == 27:
+            break
+        j+=1
+    plt.show()
+    cam.release()
+
+
+if __name__ == '__main__':
+    import sys
+
+    show_data('Crowd-Activity-All.avi', 1)
